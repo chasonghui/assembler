e557bb5ec577b63ef7e8a4391153decb766a8497
chasonghui <chasonghui@naver.com>
2018-11-13 19:55:25 +0900

    check.c change

--- check.c
+++ check.c
@@ -10,6 +10,12 @@ int is_valid(char *op, char *args)
 	/*         syntax check         */
     /*  							*/
 	/********************************/
-	
+	char tmp[256];
+	char *start = strtok(temp,",");
+	char *dest=strtok(NULL,",");
+	strcpy(tmp, args);
+	if(dest[0]=='$')return 0;
+	if((start[0]=='0'||start[0]=='('||start[0]=='-')&&(dest[0]=='-'||dest[0]=='('))
+	exit(0);
 	return 1;
 }
