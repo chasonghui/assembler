4ffbbf6c19d01fdeea649f71b584ad1c6fa96246
chasonghui <chasonghui@naver.com>
2018-11-13 22:09:57 +0900

    complete

--- trans.c
+++ trans.c
@@ -26,10 +26,10 @@ int instr_trans(char *op, char *args, char* mcode)
 	case '0':
 		if(strchr(start.op,'(')==NULL) 
 		{
-		start.type=abst;
+			start.type=abst;
 		}
 		else
-		start.type = dis;
+			start.type = dis;
 		break;
 	case '(':
 		start.type=rel;
@@ -43,15 +43,17 @@ int instr_trans(char *op, char *args, char* mcode)
 	case '-':
 		start.type =dis;
 		break;
+	default :
+		break;
 	}
 	//=======================================
 	switch(dest.op[0])
 	{
 	case '0':
 		if(strchr(dest.op,'(')==NULL)
-		dest.type= abst;
+			dest.type= abst;
 		else
-		dest.type=dis;
+			dest.type=dis;
 		break;
 	case '(':
 		dest.type = rel;
@@ -66,38 +68,40 @@ int instr_trans(char *op, char *args, char* mcode)
 		dest.type = dis;
 		break;
 	default:
-		printf("error\n");
 		break;	
 	}
 	//1. register to register
 	if(start.type ==reg)
 	{
-	if(dest.type ==reg) strcpy(mcode,"89");
+		if(dest.type ==reg) strcpy(mcode,"89");
 	//5. register to register[eax]
-    else if(dest.type==abst||dest.type==rel||dest.type==dis)strcpy(mcode,"a3");
+		else if(dest.type==abst||dest.type==rel||dest.type==dis)strcpy(mcode,"a3");
+	}
 	//6. immediate to register
 	else if(start.type==imm){
-	if(strcmp(dest.op,"%eax")==0)strcpy(mcode,"b8");
-	else if(strcmp(dest.op,"%ecx")==0)strcpy(mcode,"b9");
-	else if(strcmp(dest.op,"%edx")==0)strcpy(mcode,"ba");
-	else if(strcmp(dest.op,"%ebx")==0)strcpy(mcode,"bb");
-	else if(strcmp(dest.op,"%esp")==0)strcpy(mcode,"bc");
-	else if(strcmp(dest.op,"%ebp")==0)strcpy(mcode,"bd");
-	else if(strcmp(dest.op,"%esi")==0)strcpy(mcode,"be");
-	else if(strcmp(dest.op,"%edi")==0)strcpy(mcode,"bf");
-
+		if(strcmp(dest.op,"%eax")==0)strcpy(mcode,"b8");
+		else if(strcmp(dest.op,"%ecx")==0)strcpy(mcode,"b9");
+		else if(strcmp(dest.op,"%edx")==0)strcpy(mcode,"ba");
+		else if(strcmp(dest.op,"%ebx")==0)strcpy(mcode,"bb");
+		else if(strcmp(dest.op,"%esp")==0)strcpy(mcode,"bc");
+		else if(strcmp(dest.op,"%ebp")==0)strcpy(mcode,"bd");
+		else if(strcmp(dest.op,"%esi")==0)strcpy(mcode,"be");
+		else if(strcmp(dest.op,"%edi")==0)strcpy(mcode,"bf");
+		else strcpy(mcode,"??");
 	}
 	//3. memory to register
-	else if(dest.type==reg){
-	if(start.type==dis) strcpy(mcode,"8b");
-	}
-	else
+	else if(dest.type==reg)
 	{
-	//4.memory to register[eax]
-	if(strcmp(dest.op,"%eax")==0) strcpy(mcode,"a1");
-	//2.memory to register
-	else strcpy(mcode,"8b");
+		if(start.type==dis) strcpy(mcode,"8b");
+		
+		else
+		{
+		//4.memory to register[eax]
+			if(strcmp(dest.op,"%eax")==0) strcpy(mcode,"a1");
+			//2.memory to register
+			else strcpy(mcode,"8b");
+		}
 	}
-}
+
 	return 1;	
 }
